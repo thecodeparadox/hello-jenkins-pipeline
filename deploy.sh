c3138a4712099579da3424210007790c09af112e
thecodeparadox <dabdullahy@gmail.com>
2017-04-05 13:38:16 +0600

    Update Deployment script and .gitignor

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 printf "\nGit pulling...\n"
 git pull origin master
+
 if [ $? -eq 0 ]; then
-    printf "\Git Pull Done.\n"
+    printf "\nGit Pull Done.\n"
 else
-    printf "\Git Pull Failed\n"
+    printf "\nGit Pull Failed.\n"
 fi
 
 printf "\nLast 3 Git logs:\n"
 git log --pretty=oneline | head -n 3
 
 printf "\nReloading App...\n"
-pm2 reload ProdataRtbApi --update-env
+pm2 reload process.config.json --update-env
+
 if [ $? -eq 0 ]; then
     printf "\nApp Reloaded Successfully\n"
 else
